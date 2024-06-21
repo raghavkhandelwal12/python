70eccdf34b60d6ab4695a73a580d158e34da19fd
Raghav khandelwal <131291883+raghavkhandelwal12@users.noreply.github.com>
2024-06-21 14:11:49 +0530

    #solved question

--- question5.py
+++ question5.py
@@ -1,14 +1,30 @@
+# import os
+
+# def presentdir(path):
+#     items = os.listdir(path)
+#     file_count = 0
+#     for item in items:
+#          os.path.isfile(os.path.join(path, item))
+#          file_count += 1
+    
+#     return file_count
+
+# path = "C:\\Users\\pc\\OneDrive\\Pictures"
+# print(presentdir(path))
+# print(os.listdir())
+
+
+
 import os
 
 def presentdir(path):
-    items = os.listdir(path)
-    file_count = 0
-    for item in items:
-         os.path.isfile(os.path.join(path, item))
-         file_count += 1
-    
-    return file_count
+    file_count = len([item for item in os.listdir(path) if os.path.isfile(os.path.join(path, item))])
+    directory_contents = os.listdir(path)
+    return file_count, directory_contents
 
-path = 'c:\\Users\\pc\\OneDrive\\Documents\\raghav.py'
-print(presentdir(path))
-print(os.listdir())
+path = "C:\\Users\\pc\\OneDrive\\Documents"
+file_count, directory_contents = presentdir(path)
+print(f"Number of files: {file_count}")
+print("Contents of the directory:", directory_contents)
+
+
