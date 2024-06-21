791ca44c7da3cf5fefae5a12c44eef46d076841d
Raghav khandelwal <131291883+raghavkhandelwal12@users.noreply.github.com>
2024-06-21 14:14:55 +0530

    Update practice4.py

--- practice4.py
+++ practice4.py
@@ -1,20 +1,3 @@
-# import os
-
-# def presentdir(path):
-#     items = os.listdir(path)
-#     file_count = 0
-#     for item in items:
-#          os.path.isfile(os.path.join(path, item))
-#          file_count += 1
-    
-#     return file_count
-
-# path = "C:\\Users\\pc\\OneDrive\\Pictures"
-# print(presentdir(path))
-# print(os.listdir())
-
-
-
 import os
 
 def presentdir(path):
